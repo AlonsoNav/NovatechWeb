7d07a2ee82b7c30acb9c8174c5b629702399e399
Alonso <alonavarro@estudiantec.cr>
2024-06-07 13:51:29 -0600

    Add: Add resource GUI and connection

--- src/views/projects/ProjectResources.jsx
+++ src/views/projects/ProjectResources.jsx
@@ -11,7 +11,7 @@ import {faAdd, faFilter, faSearch, faTrash} from "@fortawesome/free-solid-svg-ic
 // Local imports
 import {useAuth} from "../../contexts/AuthContext.jsx";
 import ToastComponent from "../../components/ToastComponent.jsx";
-import {getRequest} from "../../controllers/Database.jsx";
+import {getRequest, postRequest} from "../../controllers/Database.jsx";
 import Resource from "../../models/Resource.jsx";
 import {filterByCheckbox, filterBySearchTerm} from "../../controllers/Filters.jsx";
 // React imports
@@ -21,6 +21,8 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Modal from "react-bootstrap/Modal";
+import Task from "../../models/Task.jsx";
 
 const ProjectResources = ({projectName, responsible}) => {
     ProjectResources.propTypes = {
@@ -38,9 +40,12 @@ const ProjectResources = ({projectName, responsible}) => {
     const [searchTerm, setSearchTerm] = useState("")
     const [filteredResources, setFilteredResources] = useState([])
     const [selectedTypes, setSelectedTypes] = useState([])
+    // Form data
+    const [name, setName] = useState("")
+    const [type, setType] = useState(types[0])
+    const [description, setDescription] = useState("")
     // Components
-    const [isAddForm, setIsAddForm] = useState(true) // If it's false, it's an edit form
-    const [showFormModal, setShowFormModal] = useState(false)
+    const [showAddModal, setShowAddModal] = useState(false)
     const [showToast, setShowToast] = useState(false)
     const [toastMessage, setToastMessage] = useState("")
     const [toastBg, setToastBg] = useState("danger")
@@ -100,6 +105,48 @@ const ProjectResources = ({projectName, responsible}) => {
         setFilteredResources(filteredResources)
     }, [resources, selectedTypes, searchTerm])
 
+    // Add resource
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        const form = e.currentTarget
+        if(form.checkValidity() === false)
+            return
+        let payload = {name, description, type}
+        try{
+            let response = await postRequest(payload, `proyectos/${projectName}/recursos`)
+
+            if (!response){
+                setToastMessage("Could not connect to the server.")
+                setToastBg("danger")
+                setShowToast(true)
+            }
+            else{
+                const body = await response.json()
+                if (!response.ok)
+                    setToastBg("danger")
+                else{
+                    setToastBg("info")
+                    const newResource = new Resource(
+                        name,
+                        description,
+                        type
+                    )
+                    setResources(prevResources => [...prevResources, newResource])
+                }
+                setToastMessage(body.message)
+                setShowToast(true)
+            }
+        }catch (error){
+            console.log(error)
+        }
+    }
+
+    // Platform selector
+    const typeOptions = types.map((type, index) => (
+        <option key={`type_${index}`} value={type}>{type}</option>
+    ))
+
     // Resource cards
     const resourceCards = filteredResources.map((resource, index) => (
         <Col key={`meeting_card_${index}`} >
@@ -157,6 +204,61 @@ const ProjectResources = ({projectName, responsible}) => {
                 onClose={() => setShowToast(false)}
                 bg={toastBg}
             />
+            <Modal show={showAddModal} onHide={() => setShowAddModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Resource</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form noValidate onSubmit={handleSubmit}>
+                        <Row md={2} xs={1}>
+                            <Col>
+                                <Form.Group className={"mb-3"}>
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter the name of the resource..."
+                                        maxLength={50}
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        required
+                                        isInvalid={name.length === 0}
+                                    />
+                                    <Form.Control.Feedback type='invalid'>
+                                        Please enter a valid name.
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className={"mb-3"}>
+                                    <Form.Label>Type</Form.Label>
+                                    <Form.Select value={type} onChange={(e) => setType(e.target.value)}>
+                                        {typeOptions}
+                                    </Form.Select>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Description</Form.Label>
+                                    <textarea
+                                        className="form-control"
+                                        rows={2}
+                                        placeholder="Enter the description of the resource..."
+                                        value={description}
+                                        onChange={(e) => {setDescription(e.target.value)}}
+                                        maxLength={100}
+                                        required
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <div className={"text-end"}>
+                            <button type="submit" className={"btn btn-primary mt-5"}>Add resource</button>
+                        </div>
+                    </Form>
+                </Modal.Body>
+            </Modal>
             <Offcanvas show={showOffcanvas} onHide={() => setShowOffcanvas(false)} className={"m-header custom-scrollbar"}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Filters</Offcanvas.Title>
@@ -183,8 +285,7 @@ const ProjectResources = ({projectName, responsible}) => {
                     <Col className={"text-end col-auto mt-1"}>
                         <Button className={"btn btn-primary justify-content-center"}
                                 onClick={() => {
-                                    setShowFormModal(true)
-                                    setIsAddForm(true)
+                                    setShowAddModal(true)
                                 }}>
                             <FontAwesomeIcon icon={faAdd} className={"me-2"}/>
                             Add Resources
